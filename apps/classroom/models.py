e4d5029a755c2103028ef0314a9492621c34b96f
Jendrik Soltau <jendrik.soltau90@gmail.com>
2024-09-27 10:03:03 +0200

    updates to models

--- apps/classroom/models.py
+++ apps/classroom/models.py
@@ -12,20 +12,47 @@ school_days = {
     "Sat":"Saturday"
 }
 
+school_subjects = {
+    "Math":"Math",
+    "German":"German",
+    "Eng":"English",
+    "Bio":"Biology",
+    "Chem":"Chemistry",
+    "PE":"Physical Education",
+    "HST":"History",
+    "Art":"Art",
+    "Geo":"Geography",
+    "Phi":"Philosophy",
+    "Eth":"Ethics",
+    "Rel":"Religion",
+    "Spa":"Spanish",
+    "Fre":"French",
+    "Lat":"Latin",
+}
+
 class ClassEnvironment(models.Model):
     teacher = models.ManyToManyField('userprofile.Teacher')
     class_name = models.CharField(max_length=5)
 
+    def __str__(self):
+        return f'{self.class_name}'
+
 class Lesson(models.Model):
-    subject_name = models.CharField(max_length=30)
-    teacher_id = models.ForeignKey('userprofile.Teacher', on_delete=models.CASCADE)
-    class_id = models.OneToOneField('classroom.ClassEnvironment', on_delete=models.CASCADE)
+    subject_name = models.CharField(max_length=30, choices=school_subjects)
+    teacher = models.ForeignKey('userprofile.Teacher', on_delete=models.CASCADE)
+    classenvironment = models.ForeignKey('classroom.ClassEnvironment', on_delete=models.CASCADE)
     start_time = models.TimeField()
     end_time = models.TimeField()
     day = models.CharField(choices=school_days, max_length=10)
+
+    def __str__(self):
+        return f'{self.start_time} - {self.end_time} {self.subject_name}: {self.teacher}'
 
 class Notification(models.Model):
     name = models.CharField(max_length=30)
     date = models.DateTimeField()
     description = models.TextField()
     class_id = models.ForeignKey('classroom.ClassEnvironment', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} - {self.date}'
